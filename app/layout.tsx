a75d6ef667ee0881d0363e6172c05d6dcbf74e29
Mebo <samkharadzemerab@gmail.com>
2024-12-14 22:56:59 +0400

    add navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,11 @@ export default function RootLayout({
               <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
                 <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
                   <div className="flex gap-5 items-center font-semibold">
-                    <Link href={"/"}>Agora</Link>
+                    <Link className="text-2xl" href={"/"}>
+                      Agora
+                    </Link>
+                    <Link href={"/protected/posts"}>Posts</Link>
+                    <Link href={"/protected/products"}>Products</Link>
                     <div className="flex items-center gap-2"></div>
                     <ThemeSwitcher />
                   </div>
