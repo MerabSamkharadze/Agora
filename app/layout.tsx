c2382837b83148b8964c588b1b818f8af01d04e3
Mebo <samkharadzemerab@gmail.com>
2024-12-15 11:53:40 +0400

    update meta data and favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,8 +12,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Agora",
+  description: "Give Wings to Your Talent",
 };
 
 export default function RootLayout({
